eca4f27d9082542ec95197e3e549af9bea444a4c
niglesias-sre <85466985+niglesias-sre@users.noreply.github.com>
2021-06-07 00:58:37 -0300

    Add comments to tf code

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
+# Set up AWS provider
+# Credentials must be set up using environment variables
 provider "aws" {
   region = "us-east-1"
 }
 
+# Create a S3 bucket to store files
 resource "aws_s3_bucket" "b1" {
   acl = "private"
   tags = {
@@ -10,13 +13,14 @@ resource "aws_s3_bucket" "b1" {
   }
 }
 
+# Create two files containing the timestamp when the
+# code was executed
 resource "aws_s3_bucket_object" "test1" {
   bucket  = aws_s3_bucket.b1.id
   key     = "test1.txt"
   content = timestamp()
   acl     = "public-read"
 }
-
 resource "aws_s3_bucket_object" "test2" {
   bucket  = aws_s3_bucket.b1.id
   key     = "test2.txt"
